1b6af065c3dcde105de3bf8bdf01d41c587b1b0a
mani0432 <42490321+mani0432@users.noreply.github.com>
2021-11-23 10:07:55 +0530

    Add files via upload

---
+++ AirIndiaAutomation/src/main/java/AirIndia/AirIndiaAutomation/App.java
@@ -0,0 +1,13 @@
+package AirIndia.AirIndiaAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
